b005281a16d2dffd8ce043bfb0e89279009e32c9
Fernando Yonfa <fyonfa@gmail.com>
2021-10-31 13:38:18 +0000

    new vot and increment

--- go/election-day/election_day.go
+++ go/election-day/election_day.go
@@ -1,19 +1,33 @@
 package electionday
 
+import "fmt"
+
 // NewVoteCounter returns a new vote counter with
 // a given number of inital votes.
 func NewVoteCounter(initialVotes int) *int {
-	panic("Please implement the NewVoteCounter() function")
+	//var pointerToVotes *int
+	//pointerToVotes = &initialVotes
+	//return pointerToVotes
+	return &initialVotes
+
 }
 
 // VoteCount extracts the number of votes from a counter.
 func VoteCount(counter *int) int {
-	panic("Please implement the VoteCount() function")
+	if counter == nil {
+		return 0
+	}
+	return *counter // dereferencing
 }
 
 // IncrementVoteCount increments the value in a vote counter
 func IncrementVoteCount(counter *int, increment int) {
-	panic("Please implement the IncrementVoteCount() function")
+	numberInCounter := *counter
+	fmt.Println("counter as it comes", counter)
+	newVotesInCounter := numberInCounter + increment
+	fmt.Println("new votes in counter", &newVotesInCounter)
+	*counter = newVotesInCounter
+	fmt.Println("counter at the end", counter)
 }
 
 // NewElectionResult creates a new election result

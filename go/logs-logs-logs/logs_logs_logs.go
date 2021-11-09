f146de0ab497f2bb9ebbb3cdeb8bf115b955909a
Fernando Yonfa <fyonfa@gmail.com>
2021-11-09 14:19:58 +0000

    first exercise

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,48 +1,22 @@
 package logs
 
-import "fmt"
-
 // Application identifies the application emitting the given log.
 func Application(log string) string {
 
-	reference := "!🔍☀"
-	reference2 := '!'
-
-	for _, char := range reference {
-		fmt.Println("this is reference", char)
-	}
-	fmt.Printf("reference2 %d %c %U\n", reference2, reference2, reference2)
-
 	myMapApplication := map[rune]string{
-		'!': "recommendation",
+		'❗': "recommendation",
 		'🔍': "search",
 		'☀': "weather",
 	}
-	myMapApplication2 := map[string]string{
-		"!": "recommendation",
-		"🔍": "search",
-		"☀": "weather",
-	}
-	test := "!"
-	myRune := []rune(log)
-	v, _ := fmt.Printf("%s\n", test)
-	fmt.Println(v)
 
-	fmt.Println(myRune)
-	fmt.Println(myMapApplication)
-	fmt.Println(myMapApplication2)
-
-	for index, char := range log {
-		fmt.Printf("%d, %c, %U, %d\n", index, char, char, char)
-		fmt.Println("THIS is char", char)
-
-		//convert into rune and then find it and print it
+	for _, char := range log {
 		value, exist := myMapApplication[char]
-		fmt.Println(value, exist)
-
+		if exist {
+			return value
+		}
 	}
 
-	return ""
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
